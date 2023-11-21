fd10a982349496b48ac7cdf991fa042cd927b2ba
unknown <c0b22047ae@edu.teu.ac.jp>
2023-11-21 15:56:27 +0900

    練習6

--- pygame_image.py
+++ pygame_image.py
@@ -14,12 +14,13 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])#練習4:背景画像の表示
+        x = tmr%1600
+        screen.blit(bg_img, [-x, 0])#練習4:背景画像の表示
+        screen.blit(bg_img,[1600-x,0])#練習S6:動く背景画像
         screen.blit(tori_imgs[tmr%2],[300,200])#練習5:
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
